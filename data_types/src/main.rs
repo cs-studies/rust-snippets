c0dab443918bbcbbfdbb2f4ab558202a6f64e858
YugaEgo <yet@ego.team>
2023-02-18 12:04:55 +0200

    Add example output of integers values

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -19,5 +19,28 @@ fn integers_values() {
     println!("i32: {} - {}", i32::min_value(), i32::max_value());
     println!("i64: {} - {}", i64::min_value(), i64::max_value());
     println!("i128: {} - {}", i128::min_value(), i128::max_value());
-    println!("isize: (arch-dependent) {} - {}", isize::min_value(), isize::max_value());
+    println!(
+        "isize: (arch-dependent) {} - {}",
+        isize::min_value(),
+        isize::max_value()
+    );
+
+    // *** EXAMPLE OUTPUT ***
+    // *** Integer Types Values ***
+
+    // Unsigned variants can store numbers from 0 to 2ⁿ - 1
+    // u8: 0 - 255
+    // u16: 0 - 65535
+    // u32: 0 - 4294967295
+    // u64: 0 - 18446744073709551615
+    // u128: 0 - 340282366920938463463374607431768211455
+    // usize: (arch-dependent) 0 - 18446744073709551615
+
+    // Signed variants can store numbers from -2ⁿ⁻¹ to 2ⁿ⁻¹ - 1
+    // i8: -128 - 127
+    // i16: -32768 - 32767
+    // i32: -2147483648 - 2147483647
+    // i64: -9223372036854775808 - 9223372036854775807
+    // i128: -170141183460469231731687303715884105728 - 170141183460469231731687303715884105727
+    // isize: (arch-dependent) -9223372036854775808 - 9223372036854775807
 }
