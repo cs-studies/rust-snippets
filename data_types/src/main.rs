a043fdc6d8cae44053c322f72dcc68465c5f09b0
YugaEgo <yet@ego.team>
2023-02-21 17:02:06 +0200

    Use integer MIN and MAX constants
    The previously used functions were deprecated.
    For more details, see
    https://rust-lang.github.io/rfcs/2700-associated-constants-on-ints.html

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -6,24 +6,20 @@ fn integers_values() {
     println!("\n*** Integer Types Values ***");
 
     println!("\nUnsigned variants can store numbers from 0 to 2ⁿ - 1");
-    println!("u8: 0 - {}", u8::max_value());
-    println!("u16: 0 - {}", u16::max_value());
-    println!("u32: 0 - {}", u32::max_value());
-    println!("u64: 0 - {}", u64::max_value());
-    println!("u128: 0 - {}", u128::max_value());
-    println!("usize: (arch-dependent) 0 - {}", usize::max_value());
+    println!("u8: 0 - {}", u8::MAX);
+    println!("u16: 0 - {}", u16::MAX);
+    println!("u32: 0 - {}", u32::MAX);
+    println!("u64: 0 - {}", u64::MAX);
+    println!("u128: 0 - {}", u128::MAX);
+    println!("usize: (arch-dependent) 0 - {}", usize::MAX);
 
     println!("\nSigned variants can store numbers from -2ⁿ⁻¹ to 2ⁿ⁻¹ - 1");
-    println!("i8: {} - {}", i8::min_value(), i8::max_value());
-    println!("i16: {} - {}", i16::min_value(), i16::max_value());
-    println!("i32: {} - {}", i32::min_value(), i32::max_value());
-    println!("i64: {} - {}", i64::min_value(), i64::max_value());
-    println!("i128: {} - {}", i128::min_value(), i128::max_value());
-    println!(
-        "isize: (arch-dependent) {} - {}",
-        isize::min_value(),
-        isize::max_value()
-    );
+    println!("i8: {} - {}", i8::MIN, i8::MAX);
+    println!("i16: {} - {}", i16::MIN, i16::MAX);
+    println!("i32: {} - {}", i32::MIN, i32::MAX);
+    println!("i64: {} - {}", i64::MIN, i64::MAX);
+    println!("i128: {} - {}", i128::MIN, i128::MAX);
+    println!("isize: (arch-dependent) {} - {}", isize::MIN, isize::MAX);
 
     // *** EXAMPLE OUTPUT ***
     // *** Integer Types Values ***
